bbf34b4b1707791138600684968a0f9420498c3a
valeron-developer <v.v.halatenko@gmail.com>
2018-04-04 10:30:43 +0300

    change from ms to lookup

---
+++ home_work3[cache_table]/ebin/home_work3.app
@@ -0,0 +1,8 @@
+{application, 'home_work3', [
+	{description, "New project"},
+	{vsn, "0.1.0"},
+	{modules, ['my_cache']},
+	{registered, []},
+	{applications, [kernel,stdlib]},
+	{env, []}
+]}.
--- home_work3[cache_table]/src/my_cache.erl
+++ home_work3[cache_table]/src/my_cache.erl
@@ -1,12 +1,13 @@
 % пишем библиотеку для кеширования:
 
 -module(my_cache).
--include_lib("stdlib/include/ms_transform.hrl").
+-include_lib("stdlib/include/ms_transform.hrl"). % <- библиотека для использования Match Spesification (MS)
 -export([create/0, insert/3, lookup/1, delete_obsolete/0]).
 -define(NOW, calendar:datetime_to_gregorian_seconds(erlang:localtime())).
 
 % функция создает новую таблицу для записей, с помощью ets(кеш-таблица):
-create() -> ets:new(new_table, [named_table]), ok.
+create() -> 
+	ets:new(new_table, [named_table]), ok.
 
 % функция, которая записывает данные(ключ,значение,время жизни записи) в таблицу:
 insert(Key, Value, Timer) when is_integer(Timer), Timer > 0 -> 
@@ -16,13 +17,21 @@ insert(Key, Value, Timer) when is_integer(Timer), Timer > 0 ->
 % функция, которая выводит значение по ключу, достает только не устаревшие данные:
 lookup(Key) ->	
 	Now = ?NOW,
-	MS = ets:fun2ms(fun({KeyGen, Value, TimeOut}) 
-						when Key =:= KeyGen, TimeOut > Now ->
-					Value end),
-	ets:select(new_table, MS).
+		case
+		ets:lookup(new_table, Key) of
+		[{_, Value, TimeOut}] when TimeOut >= Now ->
+			{ok, Value};
+		_ -> {error, not_found}
+		end.
+
+% закоментированый ниже код можно использовать вместо кода ets:lookup:
+	%MS = ets:fun2ms(fun({KeyGen, Value, TimeOut}) 
+	%	when Key =:= KeyGen, TimeOut > Now ->
+	%	Value end),
+	%ets:select(new_table, MS).
  
 % очистка утстаревших данных:
 delete_obsolete() ->
 	Now = ?NOW,
 	ets:select_delete(new_table, ets:fun2ms(
-			fun({_, _, TimeOut}) when TimeOut =< Now -> true end)).
+		fun({_, _, TimeOut}) when TimeOut =< Now -> true end)).
